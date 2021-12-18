0b6d6bee30b8c84b57917a11bde42aacce421f23
Altair N1 <altairn1@mail.ru>
2021-12-18 23:42:29 +0300

    #2 Удалил артифакты после теста git

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     myClient client("localhost", 2324);
-    cout << "hello";
-    cout << "Nastya" << "\n";
     client.show();
     return app.exec();
 }
