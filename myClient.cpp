a5e126b2eebf2f17ed490f994fc3f156095c7d2b
Василий <66752802+AltairN1@users.noreply.github.com>
2021-12-18 23:39:28 +0300

    hello! (#1)

--- myClient.cpp
+++ myClient.cpp
@@ -29,6 +29,7 @@ myClient::myClient(const QString& strHost, int nPort, QWidget* pwgt) :
     pvbxLayout->addWidget(m_ptxtInput);
     pvbxLayout->addWidget(pcmd);
     setLayout(pvbxLayout);
+    cout << "myClient";
 }
 
 void myClient::slotConnected(){
